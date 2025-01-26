f4b02bbdd6d683d479287206e061c0dc1da92663
Souvik Ghosh <mesouvikghosh96@gmail.com>
2025-01-26 12:08:26 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
+function Home() {
+  const [valuaTion, setValuation] = useState('')
+  const [amount, setAmount] = useState('');
+  const [equity, setEquity] = useState('');
+  
+  const submiHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  const calculation = () => {
+    const a = parseFloat(amount)
+    const e = parseFloat(equity)
+
+    const v = a / (e/100)
+
+    if (v >= 10000000) {
+      setValuation(`${(v / 10000000).toFixed(2)} Crore`)
+    } else if (v >= 100000) {
+      setValuation(`${(v / 100000).toFixed(2)} Lakh`)
+    } else if (v >= 1000) {
+      setValuation(`${(v / 1000).toFixed(2)} Thousand`)
+    }
+
+  }
+
+  const clear = () =>{
+    setValuation("")
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='h-screen w-screen bg-gray-700 flex justify-center items-center'>
+      <div className='w-auto h-auto p-3 border border-gray-500 rounded-lg'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <form onSubmit={(e) => {
+          submiHandler(e)
+        }}>
+          <input type="text"
+            className='block m-2 p-2 rounded-lg w-[250px]'
+            placeholder='Amount of funding'
+            onChange={(e) => setAmount(e.target.value)}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input type="text"
+            className='block m-2 p-2 rounded-lg w-[250px]'
+            placeholder='Equity (%)'
+            onChange={(e) => setEquity(e.target.value)}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+
+          <input type="text"
+            className='block m-2 p-2 rounded-lg w-[250px] bg-gray-50'
+            placeholder='Valuation'
+            disabled={true}
+            value={valuaTion}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+          <button onClick={calculation} className='m-2 p-2 bg-green-500 py-1 px-2 w-[115px] rounded-lg text-white font-bold hover:bg-green-600'>Calculate</button>
+          <button onClick={clear} type="reset" className='m-2 p-2 bg-red-500 py-1 px-2 w-[120px] rounded-lg text-white font-bold hover:bg-red-600'>Reset</button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home
